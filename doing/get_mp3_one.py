4ac1098d4469bdeecb820092a96ffd0a33899950
zhuzhifei1995 <1697561221@qq.com>
2021-12-05 19:10:14 +0800

    提交

--- doing/get_mp3_one.py
+++ doing/get_mp3_one.py
@@ -29,7 +29,27 @@ def get_mp3_one(mp3_code_url: str):
             'url': pq_html('#audio_media').attr('src'),
         })
     else:
-
+        if pq_html('div.other-item').length > 0:
+            mp3_url = 'https://mp.zhizhuma.com' + pq_html('.section-wrapper').attr('val')
+            mp3_url_html = open_chrome_get_html(mp3_url)
+            pq_mp3_url_html = PyQuery(''.join([mp3_url_html.replace('</body>', '')
+                                              .replace('</html>', ''), '</body></html>', ]))
+            print(pq_mp3_url_html)
+            book_id = pq_mp3_url_html('#launch_book_id').text()
+            book_code_id = pq_mp3_url_html('#launch_cr_id').text()
+            book_items = pq_mp3_url_html('.other-item')
+            for book_item in book_items.items():
+                mp3_code_get_url = 'https://mp.zhizhuma.com/share/audio.htm?rid=' + book_item.attr('rs-id') \
+                                   + '&sign=' + book_item.attr('sign') + '&bid=' + book_id + '&cid=' + book_code_id
+                print(mp3_code_get_url)
+                code_html = requests.get(mp3_code_get_url, verify=False, timeout=(2, 5), headers=url_util.header)
+                pq_code_html = PyQuery(''.join([code_html.text.replace('</body>', '')
+                                               .replace('</html>', ''), '</body></html>', ]))
+                if pq_code_html('#audio_media').length > 0:
+                    mp3_data['mp3_list'].append({
+                        'name': pq_code_html('title').text(),
+                        'url': pq_code_html('#audio_media').attr('src'),
+                    })
     return mp3_data
 
 
